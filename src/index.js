5c7ad88b8ebedbbbabdf4b614ab637fa81ad1759
Walid Newaz <mdwalid@gmail.com>
2023-03-20 03:28:28 -0600

    Cleaned u linting errors

--- src/index.js
+++ src/index.js
@@ -11,39 +11,143 @@ import {
   setCurrentPiece, setNextPiece, setTetrisCount,
   currentLevel, setCurrentLevel, levelIncreased,
   setLevelIncreased, setGeneratedBlocksCount,
-} from './piece.js';
+} from "./piece";
 
-let canvas = document.getElementById('myCanvas');
+const canvas = document.getElementById("myCanvas");
 /** @type {CanvasRenderingContext2D}  */
-let ctx = canvas.getContext("2d");
-let nextCanvas = document.getElementById('nextCanvas');
+const ctx = canvas.getContext("2d");
+const nextCanvas = document.getElementById("nextCanvas");
 /** @type {CanvasRenderingContext2D}  */
-let nextCtx = nextCanvas.getContext("2d");
-let result_container = document.getElementsByClassName('result-container');
+const nextCtx = nextCanvas.getContext("2d");
+const result_container = document.getElementsByClassName("result-container");
 
 // ################################################################################# Globals
 var bp, p;
 var animationId;
-var playButton = document.querySelector('#controls__play');
-var score = document.querySelector('#score__text');
-var levelElement = document.querySelector('#level__text');
-var mobile__contorls_element = document.querySelector('#mobile__contorls');
-var next__block_element = document.querySelector('#next-block');
+let board;
+var playButton = document.querySelector("#controls__play");
+var score = document.querySelector("#score__text");
+var levelElement = document.querySelector("#level__text");
+var mobile__contorls_element = document.querySelector("#mobile__contorls");
+var next__block_element = document.querySelector("#next-block");
 // ################################################################################# Globals
 
 ctx.canvas.width = singleBlockSize * wellColumns;
 ctx.canvas.height = singleBlockSize * wellRows;
 
-result_container[0].style.width = '145px';
-result_container[0].style.height = singleBlockSize * wellRows + 'px';
+result_container[0].style.width = "145px";
+result_container[0].style.height = `${singleBlockSize * wellRows}px`;
 
 // ################################################################################# main
-let moves = {
-  down: bp => ({ ...bp, y: bp.y + 1 }),
-  left: bp => ({ ...bp, x: bp.x - 1 }),
-  right: bp => ({ ...bp, x: bp.x + 1 }),
-  up: bp => Board.rotate(bp),
+const moves = {
+  down: (bp) => ({ ...bp, y: bp.y + 1 }),
+  left: (bp) => ({ ...bp, x: bp.x - 1 }),
+  right: (bp) => ({ ...bp, x: bp.x + 1 }),
+  up: (bp) => Board.rotate(bp),
 };
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function keyDownHandler(event) {
+  bp = board.piece;
+  if (event.key == "ArrowDown") {
+    p = moves.down(bp);
+
+    if (board.piece.reachedBottom(p)) {
+      board.piece.freeze(p);
+      board.getNewPiece();
+    } else {
+      board.piece.move(p);
+    }
+    board.draw();
+  } else if (event.key == "ArrowLeft") {
+    p = moves.left(bp);
+    if (board.piece.move(p)) {
+      if (board.piece.reachedBottom(p)) {
+        board.piece.freeze(p);
+        board.getNewPiece();
+      }
+    }
+    board.draw();
+  } else if (event.key == "ArrowRight") {
+    p = moves.right(bp);
+    if (board.piece.move(p)) {
+      if (board.piece.reachedBottom(p)) {
+        board.piece.freeze(p);
+        board.getNewPiece();
+      }
+    }
+    board.draw();
+  } else if (event.key == "ArrowUp") {
+    bp = moves.up(bp);
+    if (board.piece.reachedBottom(bp) == 0) {
+      board.piece.move(bp);
+      board.draw();
+    }
+  }
+}
+
+function mediaEventHandler() {
+  event.preventDefault();
+  const tId = event.target.parentElement.id;
+  const Id = event.target.id;
+  bp = board.piece;
+
+  if (tId == "rotate" || Id == "rotate") {
+    bp = moves.up(bp);
+    if (board.piece.reachedBottom(bp) == 0) {
+      board.piece.move(bp);
+      board.draw();
+    }
+  }
+  if (tId == "left__div" || Id == "left__div") {
+    p = moves.left(bp);
+    if (board.piece.move(p)) {
+      if (board.piece.reachedBottom(p)) {
+        board.piece.freeze(p);
+        board.getNewPiece();
+      }
+    }
+    board.draw();
+  }
+  if (tId == "right__div" || Id == "right__div") {
+    p = moves.right(bp);
+    if (board.piece.move(p)) {
+      if (board.piece.reachedBottom(p)) {
+        board.piece.freeze(p);
+        board.getNewPiece();
+      }
+    }
+    board.draw();
+  }
+  if (tId == "down" || Id == "down") {
+    p = moves.down(bp);
+
+    if (board.piece.reachedBottom(p)) {
+      board.piece.freeze(p);
+      board.getNewPiece();
+    } else {
+      board.piece.move(p);
+    }
+    board.draw();
+  }
+}
+
+async function gameOver(text) {
+  cancelAnimationFrame(animationId);
+  ctx.save();
+  ctx.fillStyle = "rgba(0,0,0, 0.5)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.restore();
+  ctx.font = "30px Chelsea Market";
+  ctx.textAlign = "center";
+  ctx.fillText(text, canvas.width / 2, canvas.height / 2);
+  playButton.textContent = "Play";
+  document.removeEventListener("keydown", keyDownHandler);
+  mobile__contorls.removeEventListener("click", mediaEventHandler);
+}
 
 function moveDown() {
   if (frameCount - downFC > gameSpeed && !isGameOver && !isGameWon) {
@@ -61,78 +165,22 @@ function moveDown() {
   }
 }
 
-var VarkeyDownHandler = function keyDownHandler(event) {
-  bp = board.piece;
-  if (event.key == 'ArrowDown') {
-    p = moves.down(bp);
-
-    if (board.piece.reachedBottom(p)) {
-      board.piece.freeze(p);
-      board.getNewPiece();
-    } else {
-      board.piece.move(p);
-    }
-    board.draw();
-  } else if (event.key == 'ArrowLeft') {
-    p = moves.left(bp);
-    if (board.piece.move(p)) {
-      if (board.piece.reachedBottom(p)) {
-        board.piece.freeze(p);
-        board.getNewPiece();
-      }
-    }
-    board.draw();
-  } else if (event.key == 'ArrowRight') {
-    p = moves.right(bp);
-    if (board.piece.move(p)) {
-      if (board.piece.reachedBottom(p)) {
-        board.piece.freeze(p);
-        board.getNewPiece();
-      }
-    }
-    board.draw();
-  } else if (event.key == 'ArrowUp') {
-    bp = moves.up(bp);
-    if (board.piece.reachedBottom(bp) == 0) {
-      board.piece.move(bp);
-      board.draw();
-    }
-  }
-};
-
-async function gameOver(text) {
-  cancelAnimationFrame(animationId);
-  ctx.save();
-  ctx.fillStyle = "rgba(0,0,0, 0.5)";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.restore();
-  ctx.font = "30px Chelsea Market";
+async function drawLevels(level) {
+  document.removeEventListener("keydown", keyDownHandler);
+  mobile__contorls.removeEventListener("click", mediaEventHandler);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
   ctx.textAlign = "center";
-  ctx.fillText(text, canvas.width / 2, canvas.height / 2);
-  playButton.textContent = 'Play';
-  document.removeEventListener('keydown', VarkeyDownHandler);
-  mobile__contorls.removeEventListener('click', mediaEventHandler);
-}
+  ctx.font = "30px Chelsea Market";
+  ctx.fillText(`Level ${level}`, canvas.width / 2, canvas.height / 2);
+  ctx.font = "normal normal 20px Caveat";
+  ctx.fillText(`Target: ${level} Tetris`, canvas.width / 2, canvas.height / 2 + 40);
+  ctx.font = "normal normal 12px Verdana";
+  ctx.fillText("(Tetris: 4 Lines)", canvas.width / 2, canvas.height / 2 + 70);
+  await sleep(2000);
 
-// ################################################################################# main
-let board;
-function playButtonHandler() {
-  if (playButton.textContent == 'Play') {
-    resetGame();
-    playButton.textContent = 'Pause';
-    board = new Board(ctx, nextCtx, score, levelElement);
-    // well = board.getEmptyBoard();
-    setWell(board.getEmptyBoard());
-    document.addEventListener('keydown', VarkeyDownHandler);
-    mobile__contorls.addEventListener('click', mediaEventHandler);
-    animate();
-  } else
-  if (playButton.textContent == 'Pause') {
-    pauseGame();
-  } else
-  if (playButton.textContent == 'Resume') {
-    resumeGame();
-  }
+  document.addEventListener("keydown", keyDownHandler);
+  mobile__contorls.addEventListener("click", mediaEventHandler);
 }
 
 async function animate() {
@@ -158,6 +206,7 @@ async function animate() {
   }
 }
 
+// ################################################################################# main
 function resetGame() {
   // console.log('frameCount', frameCount);
   // frameCount = 0;
@@ -195,10 +244,10 @@ function resetGame() {
 }
 
 function pauseGame() {
-  playButton.textContent = 'Resume';
+  playButton.textContent = "Resume";
   cancelAnimationFrame(animationId);
-  document.removeEventListener('keydown', VarkeyDownHandler);
-  mobile__contorls.removeEventListener('click', mediaEventHandler);
+  document.removeEventListener("keydown", keyDownHandler);
+  mobile__contorls.removeEventListener("click", mediaEventHandler);
   ctx.save();
   ctx.fillStyle = "rgba(0,0,0, 0.5)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -209,109 +258,60 @@ function pauseGame() {
 }
 
 function resumeGame() {
-  playButton.textContent = 'Pause';
+  playButton.textContent = "Pause";
   animate();
-  document.addEventListener('keydown', VarkeyDownHandler);
-  mobile__contorls.addEventListener('click', mediaEventHandler);
+  document.addEventListener("keydown", keyDownHandler);
+  mobile__contorls.addEventListener("click", mediaEventHandler);
 }
 
-playButton.addEventListener('click', playButtonHandler);
+function playButtonHandler() {
+  if (playButton.textContent == "Play") {
+    resetGame();
+    playButton.textContent = "Pause";
+    board = new Board(ctx, nextCtx, score, levelElement);
+    // well = board.getEmptyBoard();
+    setWell(board.getEmptyBoard());
+    document.addEventListener("keydown", keyDownHandler);
+    mobile__contorls.addEventListener("click", mediaEventHandler);
+    animate();
+  } else
+  if (playButton.textContent == "Pause") {
+    pauseGame();
+  } else
+  if (playButton.textContent == "Resume") {
+    resumeGame();
+  }
+}
+
+playButton.addEventListener("click", playButtonHandler);
 
 // ########################################################################### media queries
 function mediaFunction(media) {
   if (media.matches) { // If media query matches
-    mobile__contorls_element.style.display = 'Flex';
+    mobile__contorls_element.style.display = "Flex";
     // next__block_element.style.display='none';
     next__block_element.style.height = "25%";
-    var mobile__contorls = document.querySelector('#mobile__contorls');
+    var mobile__contorls = document.querySelector("#mobile__contorls");
   }
 }
 
-function mediaEventHandler() {
-  event.preventDefault();
-  let tId = event.target.parentElement.id;
-  let Id = event.target.id;
-  bp = board.piece;
-
-  if (tId == 'rotate' || Id == 'rotate') {
-    bp = moves.up(bp);
-    if (board.piece.reachedBottom(bp) == 0) {
-      board.piece.move(bp);
-      board.draw();
-    }
-  }
-  if (tId == 'left__div' || Id == 'left__div') {
-    p = moves.left(bp);
-    if (board.piece.move(p)) {
-      if (board.piece.reachedBottom(p)) {
-        board.piece.freeze(p);
-        board.getNewPiece();
-      }
-    }
-    board.draw();
-  }
-  if (tId == 'right__div' || Id == 'right__div') {
-    p = moves.right(bp);
-    if (board.piece.move(p)) {
-      if (board.piece.reachedBottom(p)) {
-        board.piece.freeze(p);
-        board.getNewPiece();
-      }
-    }
-    board.draw();
-  }
-  if (tId == 'down' || Id == 'down') {
-    p = moves.down(bp);
-
-    if (board.piece.reachedBottom(p)) {
-      board.piece.freeze(p);
-      board.getNewPiece();
-    } else {
-      board.piece.move(p);
-    }
-    board.draw();
-  }
-}
-
-var media = window.matchMedia("(max-width: 600px)");
+const media = window.matchMedia("(max-width: 600px)");
 mediaFunction(media); // Call listener function at run time
 media.addListener(mediaFunction);
 
 // ########################################################## next canvas
 nextCtx.canvas.height = singleBlockSize * 4;
-nextCtx.canvas.width = '150';
+nextCtx.canvas.width = "150";
 
 function drawRules() {
   ctx.fillStyle = "white";
   ctx.textAlign = "center";
-  ctx.font = "30px Chelsea Market";
+  ctx.font = "25px Chelsea Market";
   ctx.fillText("Enjoy the Game", canvas.width / 2, 40);
 }
 
-async function drawLevels(level) {
-  document.removeEventListener('keydown', VarkeyDownHandler);
-  mobile__contorls.removeEventListener('click', mediaEventHandler);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "white";
-  ctx.textAlign = "center";
-  ctx.font = "30px Chelsea Market";
-  ctx.fillText(`Level ${level}`, canvas.width / 2, canvas.height / 2);
-  ctx.font = "normal normal 20px Caveat";
-  ctx.fillText(`Target: ${level} Tetris`, canvas.width / 2, canvas.height / 2 + 40);
-  ctx.font = "normal normal 12px Verdana";
-  ctx.fillText("(Tetris: 4 Lines)", canvas.width / 2, canvas.height / 2 + 70);
-  await sleep(2000);
-
-  document.addEventListener('keydown', VarkeyDownHandler);
-  mobile__contorls.addEventListener('click', mediaEventHandler);
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-  window.addEventListener('load', drawRules);
-  document.getElementsByClassName('hidethis')[0].style.display = "none";
-  document.getElementsByClassName('hidethis')[1].style.display = "none";
+window.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("load", drawRules);
+  document.getElementsByClassName("hidethis")[0].style.display = "none";
+  document.getElementsByClassName("hidethis")[1].style.display = "none";
 });
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
